c9b596fd544b928eaae560dc0bc2b2d34130a60a
ootsukasyouya <rikkuhose@gmail.com>
2021-12-21 00:23:54 +0900

    usersshow

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
-#  get 'users/show'
-#  resources :users, :only => [:show]
-#  devise_for :users
-#  devise_for :users, controllers: {
-#        registrations: 'users/registrations'
-#}
-
- # devise_for :users
-  devise_for :users, :controllers => {
-    :confirmations => 'users/confirmations',
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions',
-    :passwords => 'users/passwords'
-   }
-  get '/users/:id', to: 'users#show', as: 'user'
-
+  root to: 'users#show'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
