b756174483a890e274527420ad6c2eb23f284e82
ootsukasyouya <rikkuhose@gmail.com>
2021-12-20 17:10:25 +0900

    devise sign up, in, and out path implemented.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+#  get 'users/show'
+#  resources :users, :only => [:show]
+#  devise_for :users
+#  devise_for :users, controllers: {
+#        registrations: 'users/registrations'
+#}
+
+ # devise_for :users
+  devise_for :users, :controllers => {
+    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+   }
+  get '/users/:id', to: 'users#show', as: 'user'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
