aa7e499aa466bc2a104d2e1a308be5f6a7489fce
ootsukasyouya <rikkuhose@gmail.com>
2021-12-21 17:37:10 +0900

    store,user,migration

---
+++ db/migrate/20211221083020_remove_profile_from_stores.rb
@@ -0,0 +1,5 @@
+class RemoveProfileFromStores < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :users, :profile, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_21_073730) do
+ActiveRecord::Schema.define(version: 2021_12_21_083511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,10 @@ ActiveRecord::Schema.define(version: 2021_12_21_073730) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_stores_on_email", unique: true
     t.index ["reset_password_token"], name: "index_stores_on_reset_password_token", unique: true
   end
@@ -35,12 +39,11 @@ ActiveRecord::Schema.define(version: 2021_12_21_073730) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.text "profile"
-    t.integer "status"
-    t.integer "age"
-    t.string "address"
-    t.string "phone_number"
+    t.string "name", null: false
+    t.integer "status", null: false
+    t.integer "age", null: false
+    t.string "address", null: false
+    t.string "phone_number", null: false
     t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
