b6a41c9fa25f6d2c5c8ba746291956d7a0785433
ootsukasyouya <rikkuhose@gmail.com>
2021-12-21 15:54:45 +0900

    adminuser追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to: 'users#show'
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :controllers => {
+    :confirmations => 'users/confirmations',
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords'
+   }
+  get '/users/:id', to: 'users#show', as: 'user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
