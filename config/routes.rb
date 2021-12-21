a7f84950e4205c478620e58e825c337596127878
ootsukasyouya <rikkuhose@gmail.com>
2021-12-21 16:49:24 +0900

    devisestore追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
     :passwords => 'users/passwords'
-   }
+    }
+  devise_for :stores, controllers: {
+  sessions:      'stores/sessions',
+  passwords:     'stores/passwords',
+  registrations: 'stores/registrations'
+    }
   get '/users/:id', to: 'users#show', as: 'user'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_081255) do
+ActiveRecord::Schema.define(version: 2021_12_21_073730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stores", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_stores_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_stores_on_reset_password_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_12_20_081255) do
     t.integer "age"
     t.string "address"
     t.string "phone_number"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
